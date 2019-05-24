da3e3027beb6aa817f88b3f635ab0baeba00386c
selin516 <selin516@naver.com>
2019-05-24 14:46:02 +0900

    #2 콤보박스 등 이벤트 처리 코드 구현

--- CB_Choice/Form1.Designer.cs
+++ CB_Choice/Form1.Designer.cs
@@ -28,28 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cbList = new System.Windows.Forms.ComboBox();
+            this.txtList = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.lblResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cbList
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
-            this.comboBox1.TabIndex = 0;
+            this.cbList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbList.FormattingEnabled = true;
+            this.cbList.Location = new System.Drawing.Point(12, 12);
+            this.cbList.Name = "cbList";
+            this.cbList.Size = new System.Drawing.Size(121, 20);
+            this.cbList.TabIndex = 0;
+            this.cbList.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
-            // textBox1
+            // txtList
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(170, 21);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "txtList";
+            this.txtList.Location = new System.Drawing.Point(13, 39);
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(170, 21);
+            this.txtList.TabIndex = 1;
+            this.txtList.Text = "txtList";
+            this.txtList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtList_KeyPress);
             // 
             // btnAdd
             // 
@@ -59,6 +61,7 @@
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // lblResult
             // 
@@ -73,15 +76,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(306, 280);
+            this.ClientSize = new System.Drawing.Size(438, 239);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.cbList);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "메뉴 선택 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,8 +93,8 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cbList;
+        private System.Windows.Forms.TextBox txtList;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label lblResult;
     }
